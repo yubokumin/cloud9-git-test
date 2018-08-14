779cb33d556b916c507a0b6bcdc3c07a897c2eb9
EC2 Default User <ec2-user@ip-172-31-37-201.us-east-2.compute.internal>
2018-08-14 08:36:22 +0000

    test02 first commit

---
+++ test02.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+print('モジュールのロード')
+
+def test():
+    print('関数：testを呼び出しました')
+    
+if __name__ == '__main__':
+    
+    print('python-izm')
+#   print('パイソンイズム')
+    test()
